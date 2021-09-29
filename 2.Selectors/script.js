6314c00cbe77f59a9ba83653b64a31d4613660cc
ThomasBackers <thomasbackers@gmail.com>
2021-09-29 23:27:48 +0200

    last commit before sleeping

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -1,13 +1,10 @@
 const importantElements = document.querySelectorAll(".important");
-for (let importantElement of importantElements) {
+for (let importantElement of importantElements)
     importantElement.setAttribute("title", "This is an important item");
-}
+
 const imgs = document.querySelectorAll("img");
-for (let img of imgs) {
-    if (!img.classList.contains("important")) {
-        img.style.display = "none";
-    }
-}
+for (let img of imgs) if (!img.classList.contains("important")) img.style.display = "none";
+
 for (let element of document.querySelectorAll("p")) {
     console.log(element.innerHTML);
     if (element.classList.length !== 0) {
