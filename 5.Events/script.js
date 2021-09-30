2899bcacd256f5a1ac6240578773896c4b46d3bf
ThomasBackers <thomasbackers@gmail.com>
2021-09-30 15:52:57 +0200

    5.Events part 2 done

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -20,7 +20,27 @@ function clickOnSquare(e) {
     ul.appendChild(newLi);
 }
 
+// key mapping is valid for Belgian keyboard; so BEWARE if you're using US qwerty or some other ;)
+function keypress(e) {
+    switch (e.keyCode) {
+        case 32:
+            console.log(e.keyCode);
+            console.log(getElapsedTime());
+            const newLi = document.createElement("li");
+            newLi.innerHTML = `[${getElapsedTime()}] spacebar hit`;
+            ul.appendChild(newLi);
+            break;
+        case 108:
+            ul.removeChild(ul.lastElementChild);
+            break;
+        case 115:
+            displayedSquareWrapper.innerHTML = "";
+            break;
+    }
+}
+
 const actionsquares = document.querySelectorAll(".actionsquare");
 for (let actionsquare of actionsquares) {
     actionsquare.addEventListener("click", clickOnSquare);
+    document.body.addEventListener("keypress", keypress);
 }
