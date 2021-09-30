4cfc9729455e3db50b920aa2a59c11d4060a8456
ThomasBackers <thomasbackers@gmail.com>
2021-09-30 21:59:45 +0200

    6.Forms first & second parts done

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -1,9 +1,12 @@
 const inputs = document.querySelectorAll("input");
 const displayField = document.querySelector("#display-firstname");
-const displayFieldContent = displayField.innerHTML;
 
-const displayName = (e) => {
-    ;
-}
+const displayName = e => displayField.innerText = e.target.value;
+const displayHardTruth = e => {
+    const hardTruth = document.querySelector("#a-hard-truth");
+    if (e.target.value >= 18) hardTruth.style.visibility = "visible";
+    else hardTruth.style.visibility = "hidden";
+} 
 
 inputs[0].addEventListener("keyup", displayName);
+inputs[1].addEventListener("keyup", displayHardTruth);
