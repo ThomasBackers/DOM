6e31dfd55585005e416aad87146fbec8efb08ea1
ThomasBackers <thomasbackers@gmail.com>
2021-09-30 17:06:22 +0200

    commit before the break

---
+++ 6.Forms/script.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll("input");
+const displayField = document.querySelector("#display-firstname");
+const displayFieldContent = displayField.innerHTML;
+
+const displayName = (e) => {
+    ;
+}
+
+inputs[0].addEventListener("keyup", displayName);
