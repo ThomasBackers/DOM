6237566cfe0acf3e254482731faccb4233f7c85f
ThomasBackers <thomasbackers@gmail.com>
2021-09-30 10:48:52 +0200

    3.Create part.2 done

--- 3.Create/script.js
+++ 3.Create/script.js
@@ -1,13 +1,3 @@
-/** returns a random rgb color */
-const randColor = () => {
-    let color = "rgb(";
-    for (let i = 0; i < 3; i++) {
-        if (i == 2) color += Math.floor(Math.random() * 256).toString() + ")";
-        else color += Math.floor(Math.random() * 256).toString() + ", ";
-    }
-    return color;
-};
-
 const colleagues = [
     "Antoine Ghysens",
     "Aurore Remy",
@@ -28,14 +18,45 @@ const colleagues = [
     "Tiffany Dessouroux",
 ];
 
+/** returns a random rgb color */
+const randColor = () => {
+    let color = "rgb(";
+    for (let i = 0; i < 3; i++) {
+        if (i == 2) color += Math.floor(Math.random() * 256).toString() + ")";
+        else color += Math.floor(Math.random() * 256).toString() + ", ";
+    }
+    return color;
+};
+
+/** returns the opposite color of a rgb value */
+const invertColor = color => {
+    const splittedColor = color.split(",");
+    // returns ['rgb(r', ' g', ' b)']
+    // then we replace useless stuff with a void string
+    let r = splittedColor[0].replace("rgb(", "");
+    let g = splittedColor[1].replace(" ", ""); 
+    let b = splittedColor[2].replace(" ", "").replace(")", "");
+    // we also put them into an array to loop over it easily
+    const rgb = [parseInt(r), parseInt(g), parseInt(b)];
+    for (var i = 0; i < rgb.length; i++) rgb[i] = (i === 3 ? 1 : 255) - rgb[i];
+    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+}
+/*
+I know I was supposed to only display the text with a black or white color
+but that one was much more funny and I had to try it, sorry
+*/
+
 const article = document.querySelector("article");
 let newSection, newP, textToAppend;
 for (let colleague of colleagues) {
+    const sectionBackgroundColor = randColor();
+    const pColor = invertColor(sectionBackgroundColor);
     newSection = document.createElement("section");
     newP = document.createElement("p");
     textToAppend = document.createTextNode(colleague);
     newP.appendChild(textToAppend);
+    newP.style.color = pColor;
     newSection.appendChild(newP);
-    newSection.style.backgroundColor = randColor();
+    newSection.style.backgroundColor = sectionBackgroundColor;
     article.appendChild(newSection);
 }
