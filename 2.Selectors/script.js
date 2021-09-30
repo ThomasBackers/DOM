067ecadff8de4537f55cf4212b19b0376dce1436
ThomasBackers <thomasbackers@gmail.com>
2021-09-30 09:32:14 +0200

    2.Selectors done, for real this time

--- 2.Selectors/script.js
+++ 2.Selectors/script.js
@@ -5,7 +5,18 @@ for (let importantElement of importantElements)
 const imgs = document.querySelectorAll("img");
 for (let img of imgs) if (!img.classList.contains("important")) img.style.display = "none";
 
-for (let element of document.querySelectorAll("p")) {
-    if (element.hasChildNodes) for (let childNode of element.childNodes) console.log(childNode);
-    if (element.classList.length !== 0) console.log(element.getAttribute("class"));
+/** returns a random rgb color */
+const randColor = () => {
+    let color = "rgb("; 
+    for (let i = 0; i < 3; i++) {
+        if (i == 2) color += Math.floor(Math.random() * 256).toString() + ")";
+        else color += Math.floor(Math.random() * 256).toString() + ", ";
+    }
+    return color;
+}
+
+for (let p of document.querySelectorAll("p")) {
+    if (p.hasChildNodes) for (let childNode of p.childNodes) console.log(childNode);
+    if (p.classList.length !== 0) console.log(p.getAttribute("class"));
+    p.style.color = randColor();
 }
